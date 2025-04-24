1ecd22813da760f55fbfb1322dbf6d60c1139fbc
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-04-24 18:29:59 +0200

    commit

---
+++ Miventana2/src/miventana2/Miventana2.java
@@ -0,0 +1,11 @@
+package miventana2;
+
+
+public class Miventana2 {
+
+
+    public static void main(String[] args){
+      Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
